69ebb07b900633bf0c18f2cab83ef0fd24c0efd9
Daniru2007 <daniruayuka@gmail.com>
2024-01-21 22:17:06 +0530

    project started

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
-use std::io;
-use tui::{backend::CrosstermBackend, Terminal};
+use crossterm::terminal::enable_raw_mode;
+use std::{io, thread, time::Duration};
+use tui::{
+    backend::CrosstermBackend,
+    widgets::{Block, Borders},
+    Terminal,
+};
 
 fn main() -> Result<(), io::Error> {
-    let stdout = io::stdout();
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
+    enable_raw_mode()?;
+    let mut terminal = Terminal::new(CrosstermBackend::new(io::stdout()))?;
+    terminal.clear()?;
+    loop {
+        terminal.draw(|rect| {
+            let size = rect.size();
+            let block = Block::default().title("Block").borders(Borders::ALL);
+            rect.render_widget(block, size);
+        });
+        thread::sleep(Duration::from_millis(5000));
+    }
     Ok(())
 }
