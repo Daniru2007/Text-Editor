99938c1f6fca4711621ef2ce177ffeaa4eee8d20
Daniru2007 <daniruayuka@gmail.com>
2024-01-23 00:30:28 +0530

    basic functions added

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,50 @@
-use crossterm::terminal::enable_raw_mode;
+use crossterm::{
+    event::{read, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
 use std::{io, thread, time::Duration};
 use tui::{
     backend::CrosstermBackend,
-    widgets::{Block, Borders},
+    layout::{Constraint, Direction, Layout},
+    text::Text,
+    widgets::{Block, Borders, Paragraph},
     Terminal,
 };
 
 fn main() -> Result<(), io::Error> {
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let mut terminal = Terminal::new(CrosstermBackend::new(stdout))?;
     enable_raw_mode()?;
-    let mut terminal = Terminal::new(CrosstermBackend::new(io::stdout()))?;
     terminal.clear()?;
+
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        // .margin(2)
+        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
+        .split(terminal.size().unwrap());
     loop {
+        if let Event::Key(event) = read()? {
+            if event.code == KeyCode::Char('q') {
+                break;
+            }
+        }
         terminal.draw(|rect| {
-            let size = rect.size();
             let block = Block::default().title("Block").borders(Borders::ALL);
-            rect.render_widget(block, size);
-        });
-        thread::sleep(Duration::from_millis(5000));
+            rect.render_widget(block, chunks[0]);
+
+            let text = Text::raw("Hello World!");
+            let paragraph = Paragraph::new(text);
+            rect.render_widget(paragraph, chunks[1]);
+        })?;
     }
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
     Ok(())
 }
